eea3eced7b241bb64156e3cc40a334f723f3e127
Dev-Jariwala <devjariwala.j@gmail.com>
2024-12-07 22:55:22 +0530

    .env added in cra

--- client/src/assets/BACKEND_URL.js
+++ client/src/assets/BACKEND_URL.js
@@ -1,4 +1,4 @@
 // const BACKEND_URL = "http://jari.ddnsfree.com/api/";
-const BACKEND_URL = "http://localhost:80/api/";
+const BACKEND_URL = process.env.REACT_APP_BACKEND_URL;
 // const BACKEND_URL = "https://jakash-server.onrender.com/api/";
 export default BACKEND_URL;
